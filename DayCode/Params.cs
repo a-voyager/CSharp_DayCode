a44f794fc77ebd379d06a51457ac9652d6c3ffca
wuhaojie <w19961009@126.com>
2016-04-05 22:05:53 +0800

    添加Params关键字.

---
+++ DayCode/Params.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayCode
+{
+
+    class Params
+    {
+        private static int sum(params int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(1, 2, 3, 4, 5));
+            Console.WriteLine(sum());
+            Console.ReadLine();
+        }
+    }
+}
